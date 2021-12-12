ad1cb96225bec80bc935cce82d634bc3af9aa23d
Mametov23 <E.Mametov@wit.edu.pl>
2021-12-12 21:30:30 +0100

    zadanie3

---
+++ unittest-zajęcia1/zadanie3/zadanie3test.py
@@ -0,0 +1,27 @@
+import zadanie3
+import unittest
+
+class TestZadanie3(unittest.TestCase):
+    def test_is_numeric(self):
+        numm = zadanie3.is_numeric(4)
+        self.assertTrue(numm)
+
+    def test_is_negative(self):
+        self.skipTest(self)
+
+    def test_calculate_saving(self, starting_amount, monthly_payment, monthly_deductions):
+
+
+#Długo myślałem, jak to zrobić, ale tam i nie mógł wykonać
+
+       
+
+
+
+    
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
